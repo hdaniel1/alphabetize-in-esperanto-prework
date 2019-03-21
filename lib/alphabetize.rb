1bd9134a0a0b4fd68a8be624fb8c174d7c341381
Daniel Hallada <hallada.daniel@gmail.com>
2019-03-21 19:21:32 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,14 @@
+require 'pry'
+
 def alphabetize(arr)
   # code here
+  counter = 0
+  alpha_hash = {}
+  esperanto = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz".split("")
+    esperanto.each do |letter|
+      alpha_hash[letter] = counter
+      counter += 1
+    end
+    binding.pry
+
 end
